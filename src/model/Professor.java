dcf6e766eedceb09b595bdae7ef20f6f6f6eeaae
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-12 11:08:54 +0200

    Add: added new class Professor with variables and gettters and ProfDegree as enum

---
+++ src/model/Professor.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Professor {
+	//1. variables
+	private long id;
+	private String name;
+	private String surname;
+	private ProfDegree degree;
+	//2. getters
+	public long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public ProfDegree getDegree() {
+		return degree;
+	}
+	
+	//3. setters
+	//4. no args constructor
+	//5. args constructor
+	//6. toString
+
+}
