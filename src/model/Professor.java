097e58186e3deb4cc6ef80cccbc9f9a55ad42301
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-12 11:17:24 +0200

    Edit: added setters in the Professor class

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -6,6 +6,9 @@ public class Professor {
 	private String name;
 	private String surname;
 	private ProfDegree degree;
+	
+	//helper variable and no need to create set or get for it
+	private static long counter = 0;
 	//2. getters
 	public long getId() {
 		return id;
@@ -24,6 +27,47 @@ public class Professor {
 	}
 	
 	//3. setters
+	public void setId() {
+		id = counter;
+		counter++;
+	}
+	
+	public void setName(String inputName) {
+		if( (inputName != null) && (!inputName.isEmpty()) 
+			&& (inputName.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))) {
+			name = inputName;
+		}
+		else
+		{
+			name = "Unknown";
+		}
+		
+	}
+	
+	public void setSurname(String inputSurname) {
+		if( (inputSurname != null) && (!inputSurname.isEmpty()) 
+			&& (inputSurname.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))) {
+			surname = inputSurname;
+		}
+		else
+		{
+			surname = "Unknown";
+		}
+		
+	}
+	
+	public void setDegree(ProfDegree inputDegree) {
+		if(inputDegree!=null) {
+			degree = inputDegree;
+		}
+		else
+		{
+			degree = ProfDegree.unknown;
+		}
+	}
+	
+	
+	
 	//4. no args constructor
 	//5. args constructor
 	//6. toString
