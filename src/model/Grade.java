5cdd43378d9817ffe1518713cf0081b327881a07
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-13 14:05:26 +0200

    Add: added new class Grade with variables and getters

---
+++ src/model/Grade.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Grade {
+	//1.variables
+	private long id;
+	private int value;
+	private Student student;
+	private Course course;
+	
+	
+	//2.getters
+	public long getId() {
+		return id;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public Student getStudent() {
+		return student;
+	}
+	
+	public Course getCourse() {
+		return course;
+	}
+	//3.setters
+	//4.no-args constr
+	//5. arg constr
+	//6.toString
+	//7. other functions(if necessary)
+
+}
